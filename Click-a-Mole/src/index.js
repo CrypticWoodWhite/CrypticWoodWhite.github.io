87acf1bbe378171a7bc019bc214aab219980580b
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-05-26 18:53:58 -0700

    pull

--- Click-a-Mole/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./css/normalize.css";
-import "./css/skeleton.css";
-import "./css/style.css";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
